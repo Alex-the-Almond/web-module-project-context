242b284a47f06579dcf710a84cca1faabe1b7229
Alexander Aymond <lynxkhaos@gmail.com>
2022-06-17 00:36:59 -0500

    still trying to fix last bug, but should be working afterwards

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import CartContext from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
+		
 	};
-
+		console.log(ProductContext)
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart} />
+			<Navigation  />
 
 			{/* Routes */}
+		<ProductContext.Provider value={{products, addItem}}>
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
+		</ProductContext.Provider>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+			<CartContext.Provider/>
 		</div>
 	);
 }
